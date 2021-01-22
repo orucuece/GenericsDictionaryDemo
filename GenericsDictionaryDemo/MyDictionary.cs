0ad20b24843cd2482f74cf1e17d4294da00a6c7d
eceorucu <eceorucu@gmail.com>
2021-01-22 09:21:51 +0300

    Add project files.

---
+++ GenericsDictionaryDemo/MyDictionary.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsDictionary
+{
+    class MyDictionary<K, V>
+    {
+        K[] keys;
+        V[] values;
+
+        //ctor: 
+        public MyDictionary()
+        {
+            keys = new K[0];
+            values = new V[0];
+        }
+
+        public void Add(K key, V value)
+        {
+
+            K[] tempKeyArray = keys;
+            V[] tempValArray = values;
+
+            keys = new K[keys.Length + 1];
+            values = new V[values.Length + 1];
+
+            for (int i = 0; i < tempKeyArray.Length; i++)
+            {
+                keys[i] = tempKeyArray[i];
+                values[i] = tempValArray[i];
+            }
+
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+
+
+            foreach (var keyItem in keys)
+            {
+                Console.WriteLine("Key: " + keyItem);
+            }
+
+            foreach (var valItem in values)
+            {
+                Console.WriteLine("Value: " + valItem);
+            }
+
+        }
+    }
+}
---
+++ GenericsDictionaryDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GenericsDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> dictionary = new MyDictionary<int, string>();
+            dictionary.Add(34, "İstanbul");
+            Console.WriteLine("");
+            dictionary.Add(35, "İzmir");
+            Console.WriteLine("");
+            dictionary.Add(36, "Kars");
+            Console.WriteLine("");
+            dictionary.Add(37, "Kayseri");
+            Console.WriteLine("");
+
+            // Using with different types pair
+            MyDictionary<int, double> dictionary2 = new MyDictionary<int, double>();
+            dictionary2.Add(8, 7.5);   //Rounding example
+            Console.WriteLine("");
+
+        }
+    }
+}
+
